61996a03ccbfa161a75323042175a38270583f75
svjaga <46368488+svjay@users.noreply.github.com>
2024-04-12 16:14:54 +0530

    init

---
+++ js/languageChange.js
@@ -0,0 +1,359 @@
+// Define function to get text based on language and key
+var language = {
+ "english": {
+        "menu": {
+            "topTitle": "Wedding Ceremony",
+            "home": "Home",
+            "events": "Events",
+            "location": "Location",
+            "tamil":"மொழியை தேர்வு செய்யவும்",
+            "english":"choose language"
+        },
+        "hederSection": {
+            "groomName": "Janarthanam",
+            "brideName": "Saranya",
+            "content": "We Are Getting Married in",
+            "saveTheDate": "Save the date"
+        },
+        "coupleContiner": {
+            "title1": "Hello!",
+            "title2": "Join Us for Our Wedding Ceremony ",
+            "title3": "On April 21st, 2024, Krishnapuram, Thiruvannamalai",
+            "groom": {
+                "name": "S. Janarthanam, D.EEE",
+                "content": "Son of Sellaperumal and Vasantha. We are thrilled to invite you to join us in celebrating the joyous union of Janarthanam and Saranya as they embark on the beautiful journey of marriage." },
+            "bride": {
+                "name": "V. Saranya, BSc (Nursing)",
+                "content": "Daughter of Vengadesan and Vanitha. As they come together to exchange vows and begin their new chapter, we would be honored to have you witness this special moment in their lives."
+            }
+        },
+        "eventContiner": {
+            "title1": "Our Special Events",
+            "title2": "Wedding Events",
+            "event1": {
+                "eventName": "Bridal Shower",
+                "fromTime": "07:00 PM ",
+                "toTime": "08:00 PM",
+                "dateDay": "Saturday 20",
+                "monthYear": "April, 2024",
+                "description": "Join us in celebrating the bride-to-be with an elegant bridal shower filled with laughter, love, and cherished memories. Indulge in delightful refreshments, engage in heartwarming conversations, and partake in joyful festivities as we honor the soon-to-be Mrs."
+            },
+            "event2": {
+                "eventName": "Musical Concert",
+                "fromTime": "08:00 PM",
+                "toTime": "10:00 PM",
+                "dateDay": "Saturday 20",
+                "monthYear": "April, 2024",
+                "description": "Let the melodies of love and harmony fill the air as we invite you to a mesmerizing Musical Concert. Join us on this enchanting evening of Saturday, April 20, 2024, at the splendid V. Vasantha Mahal in Arani. Get ready to be captivated by soul-stirring tunes, rhythmic beats, and a night filled with musical magic. Your presence will make this musical journey even more delightful!."
+            },
+            "event3": {
+                "eventName": "Wedding Reception",
+                "fromTime": "08:00 PM",
+                "toTime": "10:00 PM",
+                "dateDay": "Saturday 20",
+                "monthYear": "April, 2024",
+                "description": "Embrace the joyous rhythm of love at our wedding reception on Saturday, April 20, 2024, held at the majestic V. Vasantha Mahal in Arani. Join us as we toast to new beginnings, surrounded by the warmth of family and friends, in a night filled with laughter, music, and love."
+            },
+            "event4": {
+                "eventName": "Wedding Knot",
+                "fromTime": "06:00 AM",
+                "toTime": "07:30 AM",
+                "dateDay": "Sunday 21",
+                "monthYear": "April, 2024",
+                "description": "Mark your calendars for a joyous union as we tie the knot on Saturday, April 21, 2024, at the magnificent V. Vasantha Mahal in Arani. Let's create unforgettable moments surrounded by the splendor of our chosen venue. Your presence will make our special day complete."
+            }
+        },
+        "galleryContiner": {
+            "title1": "Our Memories",
+            "title2": "Wedding Gallery",
+            "title3": "Explore our wedding gallery and relive the magical moments that marked the beginning of our forever. From the laughter to the tears of joy, our memories are a testament to the love we share.",
+            "quotes1": "A moment of blissful connection",
+            "quotes2": "Genuine laughter between the couple",
+            "quotes3": "A shared gaze full of affection and warmth",
+            "quotes4": "Reflecting the beauty of togetherness and happiness",
+            "quotes5": "Embracing love in a tender moment",
+            "quotes6": "Expressing closeness and connection in a candid moment"
+
+        },
+        "testimonialContiner": {
+            "title1": "Best Wishes",
+            "title2": "Wishes from Siblings",
+            "testimonial1": {
+                "name": "S.Jaganathan, MCA",
+                "designation": "Senior Software Engineer",
+                "content": "Let's come together to shower him with blessings and well wishes for a lifetime of happiness. Here's to wishing my brother and his partner all the love and happiness in the world!"
+            },
+            "testimonial2": {
+                "name": "Sisters Of Groom",
+                "designation": "",
+                "content": "Here's to wishing my brother a lifetime of love and happiness in his marriage, where he can leave behind life's struggles and embrace the abundance of love and joy from his partner."
+            },
+            "testimonial3": {
+                "name": "V. Saranya, B.Sc",
+                "designation": "Sister of Bride",
+                "content": "Here's to wishing my sister a lifetime of love and happiness in his marriage, where she can leave behind life's struggles and embrace the abundance of love and joy from her partner."
+            }
+        },
+        "footerContiner": {
+            "content": "Love is not just about finding the right person, but creating a life of happiness and bliss together."
+
+        },
+        "contactContiner": {
+            "title1": "Contact Information",
+            "title2": "Vanthavasi to Arani road ",
+            "contactInformation": {
+                "title1": "V.Vasantha Mahal, Krishnapuram",
+                "title2": "Vanthavasi to Arani road"
+            }
+        }
+    },
+    "tamil": {
+        "menu": {
+            "topTitle": "வதுவை அழைப்பிதழ்",
+            "home": "",
+            "events": "நிகழ்வுகள்",
+            "location": "இடம்",
+            "tamil":"மொழியை தேர்வு செய்யவும்",
+            "english":"choose language"
+        },
+        "hederSection": {
+            "groomName": "செ. ஜெனார்த்தனம்",
+            "brideName": "வெ. சரண்யா",
+            "content": "எங்கள் வதுவை நடக்க உள்ளது",
+            "saveTheDate": "தேதியை சேமிக்கவும்"
+        },
+        "coupleContiner": {
+            "title1": "வணக்கம்!",
+            "title2": "2024 ஆப்ரல் 21ம் தேதி, கிருஷ்ணாபுரம், திருவண்ணாமலை!",
+            "title3": "எங்கள் இல்லத் திருமண விழாவிற்கு வருகை தந்து வாழ்த்துமாறு அன்புடன் கேட்டுக் கொள்கிறோம்",
+            "groom": {
+                "name": "செ. ஜெனார்த்தனம், DEEE",
+                "content": "செல்லபெருமாள்-வசந்தா அவர்களின் குமரன் ஜெனார்த்தனம் திருமணத்தின் அழகான பயணத்தைத் தொடங்கும் இந்நிகழ்ச்சியில் ​தங்களை அழைப்பதில் நாங்கள் மகிழ்ச்சியடைகிறோம்."
+            },
+            "bride": {
+                "name": "வெ. சரண்யா, B.Sc (Nursing)",
+                "content": "வெங்கிடேசன்-வனிதா அவர்களின் குமாரத்தி சரண்யா ​​அவர்களின் வாழ்வில் புதிய அத்தியாயத்தை தொடங்கும், இந்த சிறப்புமிக்க தருணத்தை நீங்களும் கண்டு ஆசீர்வதிகுமாறு கேட்டுக் கொள்கிறோம்"
+            }
+        },
+        "eventContiner": {
+            "title1": "சிறப்பு நிகழ்வுகள்",
+            "title2": "திருமண நிகழ்வுகள்",
+            "event1": {
+                "eventName": "மணமக்கள் அழைப்பு",
+                "fromTime": "மாலை 07:00",
+                "toTime": "மாலை 08:00",
+                "dateDay": "சனிக்கிழமை 20",
+                "monthYear": "ஏப்ரல், 2024",
+                "description": "மணமக்கள் அழைக்கும் நிகழ்ச்சி நடைபெறவுள்ளது. தாங்களும் இந்த மகிழ்வான நிகழ்ச்சியில் கலந்து கொண்டு மணமக்களை வாழ்த்துமாறு வேண்டுகிறோம்."
+            },
+            "event2": {
+                "eventName": "இசை நிகழ்ச்சி",
+                "fromTime": "மாலை 08:00",
+                "toTime": "மாலை 10:00",
+                "dateDay": "சனிக்கிழமை 20",
+                "monthYear": "ஏப்ரல், 2024",
+                "description": "இந்த அழகான தருணங்களை மேலும் அழகானதாக்க இசை நிகழ்ச்சி நடைபெற உள்ளது. தாங்களும் இந்நிகழ்ச்சியில் கலந்து கொண்டு இன்பங்களை பரிமாரிக்கொள்மாறு கேட்டுக்கொள்கிறோம்."
+            },
+            "event3": {
+                "eventName": "வரவேற்பு நிகழ்ச்சி",
+                "fromTime": "மாலை 08:00",
+                "toTime": "மாலை 10:00",
+                "dateDay": "சனிக்கிழமை 20",
+                "monthYear": "ஏப்ரல், 2024",
+                "description": "சனிக்கிழமை, ஏப்ரல் 20, 2024 அன்று திருவண்ணாமலையில் உள்ள வி.வசந்தா மஹாலில் திருமண வரவேற்பு நிகழ்ச்சி நடைபெற உள்ளது. தாங்களும் இந்நிகழ்ச்சியில் கலந்து கொண்டுமணமக்களை வாழ்த்துமாறு வேண்டுகிறோம்."
+            },
+            "event4": {
+                "eventName": "சுபமுகூர்த்தம்",
+                "fromTime": "காலை 06:00",
+                "toTime": "காலை 07:30",
+                "dateDay": "ஞாயிற்றுக்கிழமை 21",
+                "monthYear": "ஏப்ரல், 2024",
+                "description": "ஞாயிற்றுக்கிழமை, ஏப்ரல் 21, 2024 அன்று திருவண்ணாமலையில் உள்ள வி.வசந்தா மஹாலில் தாலிக்கட்டு நடைபெற உள்ளது. தாங்களும் இந்நிகழ்ச்சியில் கலந்து கொண்டுமணமக்களை வாழ்த்துமாறு வேண்டுகிறோம்."
+            }
+        },
+        "galleryContiner": {
+            "title1": "எங்கள் நினைவுகள்",
+            "title2": "திருமண கேலரி",
+            "title3": "எங்கள் திருமணம் ஆரம்பியும் மாந்திரீகமான நிமித்தம் நினைவுகளை படைத்துக்கொள்ளுங்கள். சிரமங்கள் முதல் மகிழ்ச்சியை அடிப்படையாக உள்ளமையை நெருக்கிய முறைகளில் நமது நினைவுகள் எங்கள் பேராசைக்கு ஆதரவாக உள்ளன.",
+            "quotes1": "A moment of blissful connection",
+            "quotes2": "Genuine laughter between the couple",
+            "quotes3": "A shared gaze full of affection and warmth",
+            "quotes4": " Reflecting the beauty of togetherness and happiness",
+            "quotes5": "Embracing love in a tender moment",
+            "quotes6": "Expressing closeness and connection in a candid moment"
+        },
+        "testimonialContiner": {
+            "title1": "வாழ்த்தும் நெஞ்சங்கள்",
+            "title2": "சகோதரர்களின் வாழ்துக்கள்",
+            "testimonial1": {
+                "name": "செ. ஜெகனாதன், MCA",
+                "designation": "மேம்பாட்டு மென்பொருள் பொறியாளர்",
+                "content": "வாழ்நாள் முழுவதும் மகிழ்ச்சியுடன் வாழ என் சகோதரனுக்கும் அவன் துணைக்கும் உலகில் உள்ள எல்லா வளமும் நலமும் கிடைக்க வாழ்த்துகிறேன்!"
+             },
+            "testimonial2": {
+                "name": "ஆணின் சகோதரிகள்",
+                "designation": " ",
+                "content": "எனது சகோதரனின் திருமண வாழ்க்கையில் அவரது துணையிடமிருந்து ஏராளமான அன்பையும் மகிழ்ச்சியையும் பெற வாழ்த்துகிறோம்."
+            },
+            "testimonial3": {
+                "name": "வெ. லாவண்யா, B.Sc",
+                "designation": "பெண் சகோதரி",
+                "content": "எனது சகோதரனின் திருமண வாழ்க்கையில் அவரது துணையிடமிருந்து ஏராளமான அன்பையும் மகிழ்ச்சியையும் பெற வாழ்த்துகிறோம்."
+            },
+     
+    },
+    "footerContiner": {
+        "content": "காதல் எற்றையானது ஒரு சரியான நபரைக் கண்டுபிடிக்கும் போது மட்டுமே அல்ல, பகிர்ந்துகொள்ளும் இன்பம் மற்றும் சந்தோஷத்தின் ஒரு வாழ்க்கையை உருவாக்குகின்றது."
+    },
+    "contactContiner": {
+        "title1": "தொடர்பு தகவல்",
+        "title2": "வந்தவாசி - ஆரணி சாலை, கிருஷ்ணாபுரம்",
+        "contactInformation": {
+            "title1": "வி.வசந்த மஹால்",
+            "title2": "வந்தவாசி - ஆரணி சாலை, கிருஷ்ணாபுரம்",
+            "title3": "வந்தவாசி - ஆரணி சாலை, கிருஷ்ணாபுரம்"
+        }
+    }
+
+    }
+}
+// var currentLanguage = true;
+
+function getText(lang, key) {
+    const keys = key.split('.');
+    let text = language[lang];
+    // console.log("text", text);
+    // Traverse the nested object using the keys
+    for (const k of keys) {
+        text = text[k];
+        if (!text) {
+            return null; // Return null if any key is not found
+        }
+    }
+
+    return text || '';
+
+}
+
+
+// Update text content of elements
+function updateText() {
+    const langC = sessionStorage.getItem('Weddingjs') || 'english'; 
+    const lang = langC || 'english'; // Default language is English
+    const textElements = [
+        
+        { key: 'menu.topTitle', selector: '#menu-topTitle' },
+        { key: 'menu.home', selector: '#menu-home' },
+        { key: 'menu.events', selector: '#menu-events' },
+        { key: 'menu.location', selector: '#menu-location' },
+        //hederSection
+        { key: 'hederSection.groomName', selector: '#hederSectiongroomName' },
+        { key: 'hederSection.brideName', selector: '#hederSectionbrideName' },
+        { key: 'hederSection.content', selector: '#hederSectioncontent' },
+        { key: 'hederSection.saveTheDate', selector: '#hederSectionsaveTheDate' },
+
+        //coupleContiner
+        { key: 'coupleContiner.title1', selector: '#coupleContinertitle1' },
+        { key: 'coupleContiner.title2', selector: '#coupleContinertitle2' },
+        { key: 'coupleContiner.title3', selector: '#coupleContinertitle3' },
+        { key: 'coupleContiner.groom.name', selector: '#coupleContinergroomname' },
+        { key: 'coupleContiner.groom.content', selector: '#coupleContinergroomcontent' },
+        { key: 'coupleContiner.bride.name', selector: '#coupleContinerbridename' },
+        { key: 'coupleContiner.bride.content', selector: '#coupleContinerbridecontent' },
+
+        //eventContiner
+        { key: 'eventContiner.title1', selector: '#eventContinertitle1' },
+        { key: 'eventContiner.title2', selector: '#eventContinertitle2' },
+
+        { key: 'eventContiner.event1.eventName', selector: '#eventContinerevent1eventName' },
+        { key: 'eventContiner.event1.fromTime', selector: '#eventContinerevent1fromTime' },
+        { key: 'eventContiner.event1.toTime', selector: '#eventContinerevent1toTime' },
+        { key: 'eventContiner.event1.dateDay', selector: '#eventContinerevent1dateDay' },
+        { key: 'eventContiner.event1.monthYear', selector: '#eventContinerevent1monthYear' },
+        { key: 'eventContiner.event1.description', selector: '#eventContinerevent1description' },
+
+        { key: 'eventContiner.event2.eventName', selector: '#eventContinerevent2eventName' },
+        { key: 'eventContiner.event2.fromTime', selector: '#eventContinerevent2fromTime' },
+        { key: 'eventContiner.event2.toTime', selector: '#eventContinerevent2toTime' },
+        { key: 'eventContiner.event2.dateDay', selector: '#eventContinerevent2dateDay' },
+        { key: 'eventContiner.event2.monthYear', selector: '#eventContinerevent2monthYear' },
+        { key: 'eventContiner.event2.description', selector: '#eventContinerevent2description' },
+
+        { key: 'eventContiner.event3.eventName', selector: '#eventContinerevent3eventName' },
+        { key: 'eventContiner.event3.fromTime', selector: '#eventContinerevent3fromTime' },
+        { key: 'eventContiner.event3.toTime', selector: '#eventContinerevent3toTime' },
+        { key: 'eventContiner.event3.dateDay', selector: '#eventContinerevent3dateDay' },
+        { key: 'eventContiner.event3.monthYear', selector: '#eventContinerevent3monthYear' },
+        { key: 'eventContiner.event3.description', selector: '#eventContinerevent3description' },
+
+        { key: 'eventContiner.event4.eventName', selector: '#eventContinerevent4eventName' },
+        { key: 'eventContiner.event4.fromTime', selector: '#eventContinerevent4fromTime' },
+        { key: 'eventContiner.event4.toTime', selector: '#eventContinerevent4toTime' },
+        { key: 'eventContiner.event4.dateDay', selector: '#eventContinerevent4dateDay' },
+        { key: 'eventContiner.event4.monthYear', selector: '#eventContinerevent4monthYear' },
+        { key: 'eventContiner.event4.description', selector: '#eventContinerevent4description' },
+
+        //galleryContiner
+        { key: 'galleryContiner.title1', selector: '#galleryContinertitle1' },
+        { key: 'galleryContiner.title2', selector: '#galleryContinertitle2' },
+        { key: 'galleryContiner.title3', selector: '#galleryContinertitle3' },
+        { key: 'galleryContiner.quotes1', selector: '#galleryContinerquotes1' },
+        { key: 'galleryContiner.quotes2', selector: '#galleryContinerquotes2' },
+        { key: 'galleryContiner.quotes3', selector: '#galleryContinerquotes3' },
+        { key: 'galleryContiner.quotes4', selector: '#galleryContinerquotes4' },
+        { key: 'galleryContiner.quotes5', selector: '#galleryContinerquotes5' },
+        { key: 'galleryContiner.quotes6', selector: '#galleryContinerquotes6' },
+
+        //testimonialContiner
+        { key: 'testimonialContiner.title1', selector: '#testimonialContinertitle1' },
+        { key: 'testimonialContiner.title2', selector: '#testimonialContinertitle2' },
+        { key: 'testimonialContiner.testimonial1.name', selector: '#testimonialContinertestimonial1name' },
+        { key: 'testimonialContiner.testimonial1.designation', selector: '#testimonialContinertestimonial1designation' },
+        { key: 'testimonialContiner.testimonial1.content', selector: '#testimonialContinertestimonial1content' },
+        { key: 'testimonialContiner.testimonial2.name', selector: '#testimonialContinertestimonial2name' },
+        // { key: 'testimonialContiner.testimonial2.designation', selector: '#testimonialContinertestimonial2designation' },
+        { key: 'testimonialContiner.testimonial2.content', selector: '#testimonialContinertestimonial2content' },
+
+        { key: 'testimonialContiner.testimonial3.name', selector: '#testimonialContinertestimonial3name' },
+        { key: 'testimonialContiner.testimonial3.designation', selector: '#testimonialContinertestimonial3designation' },
+        { key: 'testimonialContiner.testimonial3.content', selector: '#testimonialContinertestimonial3content' },
+
+
+        //footerContiner
+        { key: 'footerContiner.content', selector: '#footerContinercontent' },
+
+        //contactContinertitle1
+        { key: 'contactContiner.title1', selector: '#contactContinertitle1' },
+        { key: 'contactContiner.title2', selector: '#contactContinertitle2' },
+
+        //contactContiner
+        { key: 'contactContiner.contactInformation.title1', selector: '#contactContinercontactInformationtitle1' },
+        { key: 'contactContiner.contactInformation.title2', selector: '#contactContinercontactInformationtitle2' },
+        { key: 'contactContiner.contactInformation.title3', selector: '#contactContinercontactInformationtitle3' },
+        
+
+    ];
+
+    // Loop through the textElements array and update text content for each element
+    textElements.forEach(element => {
+        const text = getText(lang, element.key);
+        const el = document.querySelector(element.selector);
+        if (el) {
+            // el.style.fontFamily = "Arial, sans-serif"; 
+            el.textContent = text;
+            // console.log(" el.textContent", el.textContent);
+        }
+    });
+}
+
+window.onload = function () {
+    // Add click event listener to the toggle button
+    const toggleButton = document.getElementById('toggleButton');
+    toggleButton.addEventListener('change', () => {
+        const langC = sessionStorage.getItem('Weddingjs'); 
+        const lang = langC === 'tamil' ? 'english' : 'tamil' || 'english'; // Toggle between English and Tamil
+        sessionStorage.setItem('Weddingjs', lang);
+        updateText();
+    });
+    updateText();
+};
